845d1aae0e7cacfcdcb77f12e35672509c4a1f44
MikaZ21 <zukeyamamika@gmail.com>
2022-01-03 21:59:14 -0600

    worked on exercises and studios

---
+++ src/org/launchcode/java/exercises/Alice.java
@@ -0,0 +1,29 @@
+package org.launchcode.java.exercises;
+import java.util.Scanner;
+
+public class Alice {
+    public static void main(String[] args) {
+        String rawString = "Alice was beginning to get very tired of sitting " +
+                "by her sister on the bank, and of having nothing to do: once" +
+                " or twice she had peeped into the book her sister was reading, " +
+                "but it had no pictures or conversations in it, 'and what is the " +
+                "use of a book,' thought Alice 'without pictures or conversation?'";
+
+        String firstSentence = rawString.toLowerCase();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a String to search for: ");
+        String searchTerm = input.nextLine().toLowerCase();
+
+        boolean result = firstSentence.contains(searchTerm);
+        System.out.println("The first sentence contains your search ");
+
+        int index = firstSentence.indexOf(searchTerm);
+        int length = searchTerm.length();
+        System.out.println("Your search term first appears at index "
+                + index + ". Your term is " + length + "characters long.");
+
+//        String modifiedSentence = firstSentence.replace(searchTerm, replacement: "");
+//        System.out.println(modifiedSentence);
+    }
+}
